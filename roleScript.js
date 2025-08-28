51d224444cbc48fdcbe2654e724b6a9b431f221d
SEGMENT0000 <johnsonchristopher6958@gmail.com>
2025-08-28 08:54:08 -0700

    message

---
+++ roleScript.js
@@ -0,0 +1,14 @@
+var typeData = new Typed(".role", {
+    strings: [
+      "a Discord Bot Developer",
+      "a Web Developer", 
+      "a Mobile App Developer",
+      "a Problem Solver",
+      "a Tech Enthusiast",
+      "H E L L •|• F A R D",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+  });
---
+++ scrollToTop.js
@@ -0,0 +1,19 @@
+
+// Scroll to top functionality
+window.addEventListener('scroll', function() {
+    const scrollBtn = document.getElementById('scrollToTop');
+    if (scrollBtn) {
+        if (window.pageYOffset > 300) {
+            scrollBtn.style.display = 'block';
+        } else {
+            scrollBtn.style.display = 'none';
+        }
+    }
+});
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
